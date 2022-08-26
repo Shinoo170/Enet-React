8d6901e1f15ed256cacb58f83e17f3ae38c96139
Shinoo <s6203051623081@kmutnb.ac.th>
2022-08-26 11:35:02 +0700

    add func-props.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import {Header, Content, Footer} from './func-components' 
 import MsgBox from './class-props';
+import MsgBox2 from './func-props';
 
 function App() {
   return (
     <>
       <Header/>
       <Content/>
+      <div style={{marginLeft:'12px'}}>This props class</div>
       <MsgBox 
+        text="React & Reatc Native" 
+        color="blue" 
+        bgColor="#ccc" 
+        fontSize="18pt" 
+        border="solid 1px black"
+        />
+        <div style={{marginLeft:'12px'}}>This props function</div>
+        <MsgBox2
         text="React & Reatc Native" 
         color="blue" 
         bgColor="#ccc" 
