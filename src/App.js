4a35e2d604ec936a7bc821ca2d661c43a94732ef
Shinoo <s6203051623081@kmutnb.ac.th>
2022-08-26 11:22:49 +0700

    add class-props.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import {Header, Content, Footer} from './func-components' 
+import MsgBox from './class-props';
 
 function App() {
   return (
-    [<Header/>, <Content/>, <Footer/>]
+    <>
+      <Header/>
+      <Content/>
+      <MsgBox 
+        text="React & Reatc Native" 
+        color="blue" 
+        bgColor="#ccc" 
+        fontSize="18pt" 
+        border="solid 1px black"
+        />
+      <Footer/>
+    </>
   );
 }
 
